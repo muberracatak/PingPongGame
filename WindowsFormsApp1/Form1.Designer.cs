c0066f688a04ea02a35d610528c96e898d71a880
Müberra <55498680+muberracatak@users.noreply.github.com>
2021-11-17 19:48:28 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.score = new System.Windows.Forms.Label();
+            this.points = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.points);
+            this.playground.Controls.Add(this.score);
+            this.playground.Controls.Add(this.button1);
+            this.playground.Controls.Add(this.label4);
+            this.playground.Controls.Add(this.label3);
+            this.playground.Controls.Add(this.label2);
+            this.playground.Controls.Add(this.label1);
+            this.playground.Controls.Add(this.ball);
+            this.playground.Controls.Add(this.racket);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(800, 450);
+            this.playground.TabIndex = 0;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.Color.Black;
+            this.racket.Location = new System.Drawing.Point(299, 398);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(195, 18);
+            this.racket.TabIndex = 0;
+            this.racket.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ball.Location = new System.Drawing.Point(97, 174);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(28, 24);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(-3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 441);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(-8, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(309, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(484, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(408, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(773, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 450);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(648, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Pause";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.Location = new System.Drawing.Point(65, 40);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(89, 25);
+            this.score.TabIndex = 8;
+            this.score.Text = "Score : ";
+            // 
+            // points
+            // 
+            this.points.AutoSize = true;
+            this.points.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.points.Location = new System.Drawing.Point(147, 40);
+            this.points.Name = "points";
+            this.points.Size = new System.Drawing.Size(25, 25);
+            this.points.TabIndex = 9;
+            this.points.Text = "0";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label points;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+
+
+public partial class Form1 : Form
+    {
+        public int speed_left = 4;
+        public int speed_top = 4;
+ 
+        public int point = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            Cursor.Hide();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            //this.Bounds = Screen.PrimaryScreen.Bounds;
+            racket.Top = playground.Bottom - (playground.Bottom / 10);
+        }
+
+
+   
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random sayi = new Random();
+            int formWidth, formHeight;
+            racket.Left = Cursor.Position.X - (racket.Width / 2);
+            ball.Left += speed_left;
+            ball.Top += speed_top;
+         
+
+            if (ball.Bottom >= racket.Top && ball.Bottom <= racket.Bottom && ball.Left >= racket.Left && ball.Right <= racket.Right)
+            {
+                speed_top += 0;
+                speed_left += 2;
+                speed_top = -speed_top;
+
+                point += 1;
+                points.Text = point.ToString();
+
+
+            }
+
+            if (ball.Left <= playground.Left)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Right >= playground.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Top <= playground.Top)
+            {
+                speed_top = -speed_top;
+            }
+            if (ball.Right >= label4.Left)
+            {
+                speed_left = -speed_left;
+
+            }
+
+            /* if (ball.Left >= label2.Right || ball.Right <= label3.Left)
+             {
+                 this.ball.Location = new System.Drawing.Point(sayi.Next(30), sayi.Next(70));
+
+             }*/
+            Random sayi2 = new Random();
+            //sayi2 = sayi2.Next(5);
+            string[] renkler = { "blue", "red", "green", "black" ,"yellow"};
+            if (ball.Bottom >= playground.Bottom)
+            {             
+                this.ball.Location = new System.Drawing.Point(sayi.Next(800), sayi.Next(10));
+              
+                    if (renkler[sayi2.Next(5)] == "blue")
+                    {
+                        this.ball.BackColor = Color.Blue;
+                    }
+                  
+                    if (renkler[sayi2.Next(5)] == "red")
+                    {
+                        this.ball.BackColor = Color.Red;
+                    }
+                 
+
+                    if (renkler[sayi2.Next(5)] == "green")
+                    {
+                        this.ball.BackColor = Color.Green;
+                    }
+                    
+
+                    if (renkler[sayi2.Next(5)] == "black")
+                    {
+                        this.ball.BackColor = Color.Black;
+                    }
+                  
+
+                    if (renkler[sayi2.Next(5)] == "yellow")
+                    {
+                        this.ball.BackColor = Color.Yellow;
+                    }
+                   
+
+
+
+
+            }
+
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled)
+            {
+                timer1.Enabled = false;
+                button1.Text = "Start";
+            }
+            else
+            {
+                timer1.Enabled = true;
+                button1.Text = "Pause";
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random sayi = new Random();
+            if (timer2.Enabled == true)
+
+            {
+                this.ball.Location = new System.Drawing.Point(sayi.Next(800), sayi.Next(10));
+                timer2.Enabled = false;
+
+            }
+
+            else
+
+            {
+
+                timer2.Enabled = true;
+
+            }
+        }
+    }
+}
+/*
+ * using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp4
+{
+public partial class Form1 : Form
+{
+Mover.Moverd mover;
+List movers = new List();
+public Form1()
+{
+InitializeComponent();
+}
+
+
+private void randomBall(object sender, EventArgs e)
+{
+this.WindowState = FormWindowState.Maximized;
+this.DoubleBuffered = true;
+Timer timer = new Timer();
+timer.Enabled = true;
+timer.Interval = 10;
+timer.Tick += timerTick;
+this.Paint += randomBallPaint;
+
+for (int i=0;i<5;i++)
+{
+mover = new Mover.Moverd(this.Width, this.Height);
+movers.Add(mover);
+}
+}
+
+private void randomBallPaint(object sender, PaintEventArgs e)
+{
+foreach (var item in movers)
+{
+item.Update();
+item.Display(e.Graphics);
+}
+}
+
+private void timerTick(object sender, EventArgs e)
+{
+Invalidate();
+}
+}
+}
+*/
